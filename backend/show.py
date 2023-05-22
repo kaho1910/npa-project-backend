9fb661ca28f07913351c6dce0efe7d1d1d12147f
Thirawee Thitiwattanakornsiri <51158745+kaho1910@users.noreply.github.com>
2023-05-22 16:48:43 +0700

    feat: add backend-show function

--- backend/show.py
+++ backend/show.py
@@ -1,1 +1,13 @@
-import pyats
+# import pyats
+from genie.testbed import load
+
+tb = load("../backend/tb.yaml")
+
+def show_command(device, command):
+    device = device.upper()
+    dev = tb.devices[device]
+    dev.connect(log_stdout=False)
+    return dev.parse(command)
+
+# if __name__ == '__main__':
+    # show_command("R2", "show ip interface brief")
