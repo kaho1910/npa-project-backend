1740af949ba92f94de283dd134f3f85284c87c84
Thirawee Thitiwattanakornsiri <51158745+kaho1910@users.noreply.github.com>
2023-05-22 16:50:32 +0700

    chore: importing show_command() from backend

--- api/main.py
+++ api/main.py
@@ -1,6 +1,23 @@
-from fastapi import FastAPI
+import sys
+import os
+
+# getting the name of the directory
+# where the this file is present.
+current = os.path.dirname(os.path.realpath(__file__))
+ 
+# Getting the parent directory name
+# where the current directory is present.
+parent = os.path.dirname(current)
+ 
+# adding the parent directory to
+# the sys.path.
+sys.path.append(parent)
+
+from fastapi import FastAPI, Body
 from pydantic import BaseModel
 from typing import List
+
+from backend.show import show_command
 
 app = FastAPI()
 
